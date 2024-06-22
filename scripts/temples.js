2db4675452eeb88df6b7aa1cea6141a89b4e66e9
maryoritm/wss131 <mol23006@byui.edu>
2024-06-21 22:35:34 -0600

    b

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const yearSpan = document.getElementById('year');
     const menuToggle = document.getElementById('menu-toggle');
     const navUl = document.querySelector('nav ul');
     const currentDateTimeSpan = document.getElementById('current-datetime');
 
-    
+
     // Set the year in the footer
     const currentYear = new Date().getFullYear();
     yearSpan.textContent = currentYear;
-    
-      // Get current date and time
+
+    // Get current date and time
     const now = new Date();
     const formattedDateTime = now.toLocaleString(); // Adjust formatting as needed
 
-  // Display current date and time in the footer
-  currentDateTimeSpan.textContent = formattedDateTime;
+    // Display current date and time in the footer
+    currentDateTimeSpan.textContent = formattedDateTime;
 
 
     // Set the last modified date in the footer
