e46686ec38c6750822aff3f040315b6c9891c113
maryoritm/wss131 <mol23006@byui.edu>
2024-06-28 00:00:21 -0600

    a

---
+++ scripts/place.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const yearElement = document.getElementById('currentyear');
+    const lastModifiedElement = document.getElementById('last-modified');
+
+    yearElement.textContent = new Date().getFullYear();
+    lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
+
+    const temperature = 10; // example temperature in °C
+    const windSpeed = 5; // example wind speed in km/h
+
+    const windChill = calculateWindChill(temperature, windSpeed);
+    document.getElementById('windchill-factor').textContent = windChill;
+});
+
+function calculateWindChill(temp, speed) {
+    if (temp <= 10 && speed > 4.8) {
+        return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(2) + "°C";
+    } else {
+        return 'N/A';
+    }
+}
